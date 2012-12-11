aba9b623c8caf92b6318dc0c5d71e9ba0f437fc3
iocikun.jujon <PAF01143@nifty.ne.jp>
2012-12-11 10:33:29 +0900

    refactoring quickSort.c

--- quickSort/listLib.h
+++ quickSort/listLib.h
@@ -7,3 +7,13 @@ struct elem {
 	int value;
 	struct elem *next;
 };
+
+struct list* makeList(void init(), int fun(), int len);
+struct list* single(int n);
+void concat(struct list* lst1, struct list* lst2);
+struct list* tail(struct list* lst);
+
+int empty(struct list *lst);
+
+void foreach(void fun(int), struct list* lst);
+struct list* filter(int p(int, int), int, struct list* lst);
--- quickSort/quickSort.c
+++ quickSort/quickSort.c
@@ -1,1 +1,44 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
 #include "listLib.h"
+
+struct list* quickSort(struct list* lst);
+
+void initRandom() { srand(time(NULL)); }
+int getRandom() { return rand() % 6 + 1; }
+void printInt(int n) { printf("%d ", n); }
+
+int
+main(int argc, char *argv[])
+{
+	struct list *randomList = makeList(initRandom, getRandom, 10);
+
+	foreach(printInt, randomList);
+	printf("\n");
+
+	foreach(printInt, quickSort(randomList));
+	printf("\n");
+
+	return 0;
+}
+
+int large(int n, int x) { return x > n; }
+int notLarge(int n, int x) { return x <= n; }
+
+struct list*
+quickSort(struct list* lst)
+{
+	if (empty(lst)) return lst;
+
+	int h = lst->head->value;
+	struct list *t = tail(lst);
+
+	struct list *nl = quickSort(filter(notLarge, h, t));
+	struct list *l = quickSort(filter(large, h, t));
+
+	concat(nl, single(h));
+	concat(nl, l);
+
+	return nl;
+}
